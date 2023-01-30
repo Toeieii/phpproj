30acbe66d92533a8e3894317d5a3134fcaed6530
Hacker <117435105+KaoklaDX@users.noreply.github.com>
2023-01-30 16:56:11 +0700

    add func and function1

---
+++ func.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Kanit&subset=thai,latin&#39; rel='stylesheet' type='text/css'>
+    <style>
+        div {
+            align-self: start;
+            width: 30%;
+            border: 5px solid purple;
+            padding-left: 50px;
+            padding-right: 50px;
+            padding-top: 20px;
+            margin: auto;
+            font-family: 'Kanit', sans-serif;
+        }
+        .divIn{
+            width: 30%;
+            border: 2px solid red;
+            padding-left: 30px;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h1>การใช้งาน Function.</h1>
+    <hr>
+        <?php
+            echo "User Define Function. ";
+            echo "<hr>";
+            //การสร้าง Function
+            // 1. ไม่มี Parameter และไม่มีการ Return ค่ากลับ
+            function add(){
+                $num1 = 5; // Local variable
+                $num2 = 7;
+                $sum = $num1 + $num2;
+                echo "<br>Add Function<br>";
+                echo $num1. " + ".$num2. " = ". $sum;
+            }
+            // 2. มี Parameter และไม่มีการ Return ค่ากลับ
+            function sub($num1, $num2){
+                $result = $num1 - $num2;
+                echo "<br>Subtract Function<br>";
+                echo $num1. " - ". $num2. " = ". $result;
+            }
+            // 3. ไม่มี Parameter มีการ Return ค่ากลับ
+            function mul(){
+                global $num1;
+                global $num2;
+                $num1 = 8; // Local variable
+                $num2 = 4;
+                $result = $num1 * $num2;
+                echo "<br>Multiply Function<br>";
+                return $result;
+            }
+            // 4. มี Parameter มีการ Return ค่ากลับ
+            function div($num1,$num2){
+                $result = $num1 / $num2;
+                echo "<br>Division Function<br>"; 
+                return $result;
+            }
+            // การเรียกใช้ Function (Call Function)
+            add();
+            echo "<br>";
+            sub(5,2); //มี 2 Argument
+            echo "<br>";
+            $result = mul();
+            echo $num1. " * ". $num2. " = ". $result;
+            echo "<br>";
+            $num1 = 9;
+            $num2 = 5;
+            $result = div($num1, $num2);
+            echo $num1. " / ". $num2. " = ". $result;
+            echo "</h2>";
+        ?>
+    </div>
+</body>
+</html>
---
+++ function1.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+            align-self: start;
+            width: 30%;
+            border: 5px solid green;
+            padding-left: 50px;
+            padding-top: 20px;
+            margin: auto;
+    }
+.divIn{
+            width: 30%;
+            border: 2px solid red;
+            padding-left: 30px;
+            margin: 5px;
+    }
+h2{
+    font-family: "Times New Roman", Times, serif;
+}
+</style>
+</head>
+<body>
+<div>
+<h1>User Define Function.</h1>
+<hr>
+<form action="function1.php" method="post">
+กรุณากรอกตัวเลข: <br>
+ระบุตัวเลขตัวที่ 1: <input type="text" name="num1"><br>
+ระบุตัวเลขตัวที่ 2: <input type="text" name="num2"><br>
+กรุณาเลือกโหมดประมวลผล: <br><select name="mode">
+<option value="" disabled selected>เลือกโหมด</option>
+    <option value="Add">บวก</option>
+    <option value="Subtract">ลบ</option>
+    <option value="Multiply">คูณ</option>
+    <option value="Division">หาร</option>
+</select>
+<input type="submit">
+</form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $num1 = $_POST['num1'];
+        if (empty($num1) || empty($num2)) {
+          echo "<br>***Number 1 or Number 2 is empty";
+        }
+    isset( $_POST['mode'] ) ? $mode = $_POST['mode'] : $mode = "";
+    isset( $_POST['num1'] ) ? $num1 = $_POST['num1'] : $num1 = "";
+    isset( $_POST['num2'] ) ? $num2 = $_POST['num2'] : $num2 = "";
+    settype($num1,'int');
+    settype($num2,'int');
+    echo "<h3>";
+    echo "<br>Number1 = ".$num1."<br>";
+    echo "Number2 = ".$num2."<br>";
+    echo "</h3>";
+    echo "<h1>ประมวลผลด้วยโหมด : ". $mode."</h1>";
+    //Add Function
+    function add($num1, $num2){
+        $sum = $num1 + $num2;
+        echo $num1. " + " .$num2. " = ". $sum;
+    }
+    //Subtract Function
+    function Subtract($num1, $num2){
+        $result = $num1 - $num2;
+        return $result;
+    }
+    //Multiply Function
+    function Multiply(){
+        isset( $_POST['mode'] ) ? $mode = $_POST['mode'] : $mode = "";
+        isset( $_POST['num1'] ) ? $num1 = $_POST['num1'] : $num1 = "";
+        isset( $_POST['num2'] ) ? $num2 = $_POST['num2'] : $num2 = "";
+        settype($num1,'int');
+        settype($num2,'int');
+        $total = $num1 * $num2;
+        echo $num1. " * " .$num2. " = ". $total;
+    }
+    //Multiply Function
+    function Division(){
+        isset( $_POST['mode'] ) ? $mode = $_POST['mode'] : $mode = "";
+        isset( $_POST['num1'] ) ? $num1 = $_POST['num1'] : $num1 = "";
+        isset( $_POST['num2'] ) ? $num2 = $_POST['num2'] : $num2 = "";
+        settype($num1,'int');
+        settype($num2,'int');
+        $result = $num1 / $num2;
+        return $result;
+    }
+//Mode Checking
+echo "<h1>";        
+    if($mode=="Add"){
+        add($num1, $num2); // Call Function
+    }elseif($mode=="Subtract"){
+        $result = Subtract($num1, $num2); // Call Function
+        echo $num1. " - " .$num2. " = ". $result;
+    }elseif($mode=="Multiply"){
+        Multiply(); // Call Function
+    }elseif($mode=="Division"){
+        $result = Division(); // Call Function
+        echo $num1. " / " .$num2. " = ". $result;
+    }else{
+        echo "Can't Process.";
+    }
+echo "</h1>";  
+}
+    ?>
+</div>
+
+</body>
+</html>
